eb06ae668cac9adb8dbc7809114f52be7861234e
Thien <thien881199@gmail.com>
2025-05-22 15:31:45 +0700

    cloud video folder update 5

--- signaling-server/src/services/cloudVideo.js
+++ signaling-server/src/services/cloudVideo.js
@@ -10,13 +10,16 @@ const getVideos = async (folder) => {
   try {
     const result = await cloudinary.api.resources({
       resource_type: 'video',
+      prefix: folder,
       max_results: 100,
     });
-    return result.resources.map(video => ({
+    return result 
+    ? result.resources.map(video => ({
       public_id: video.public_id,
       name: video.public_id.split('/').pop(), // Extract file name
       secure_url: video.secure_url,
-    }));
+    }))
+    : {public_id: "",name: "", secure_url:""};
   } catch (error) {
     console.error(`Cloudinary getVideos error: ${error.message}`);
     throw new Error('Failed to fetch videos');
