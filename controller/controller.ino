da2e877261462e0854431482c565b8394223383f
Thien021103 <thien881199@gmail.com>
2025-06-23 15:50:41 +0700

    draft for reed switch

--- controller/controller.ino
+++ controller/controller.ino
@@ -13,6 +13,7 @@
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 #define OLED_ADDRESS 0x3C
+#define REED_PIN 13
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 WebSocketsClient webSocket;
@@ -24,6 +25,7 @@ bool wsConfigured = false;
 String CONTROLLER_TOKEN = "";
 String WEBRTC_STATE = "Impossible";
 String DOOR_STATE = "Locked";
+bool needLockCheck = false;
 
 volatile bool buttonPressed = false;
 
@@ -39,12 +41,30 @@ void updateDisplay(String status, String token = "") {
   display.println("Door Lock Status");
   display.println(WiFi.status() == WL_CONNECTED ? "WiFi: Connected" : "WiFi: Disconnected");
   display.println(wsConnected ? "WebSocket: Connected" : "WebSocket: Disconnected");
-  display.println("Status: " + status);
+  display.println("Status: " + DOOR_STATE);
   display.println(status);
   if (token.length() > 0) {
     display.println("Token: " + token.substring(0, min(token.length(), 20U)));
   }
   display.display();
+}
+
+void attemptLock() {
+  // Door ready to be locked
+  if (digitalRead(REED_PIN) == LOW) {
+    Serial.println("Door closed, locking...");
+    doorServo.write(0);
+    DOOR_STATE = "Locked";
+    updateDisplay("Locked");
+    webSocket.sendTXT("LOCK controller " + CONTROLLER_TOKEN);
+    needLockCheck = false;
+
+  // Door can not be locked
+  } else {
+    Serial.println("Door open, waiting to lock ...");
+    updateDisplay("Waiting to lock ...");
+    needLockCheck = true;
+  }
 }
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
@@ -73,28 +93,31 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
         doorServo.write(0);  // Back to locked position
         Serial.println("Door locked again");
         DOOR_STATE = "Locked";
+        updateDisplay("Door locked");
         webSocket.sendTXT("LOCK controller " + CONTROLLER_TOKEN);
+        // attemptLock();
       }
-      else if (message == "LOCK " + String(CONTROLLER_TOKEN)) {
-        if (DOOR_STATE == "Unlocked") {
-          Serial.println("Locking door...");
-          doorServo.write(0);  // Back to locked position
-        }
+      else if (message == "LOCK " + String(CONTROLLER_ID)) {
+        Serial.println("Locking door...");
+        doorServo.write(0);  // Back to locked position
+        updateDisplay("Door locked");
+        DOOR_STATE = "Locked";
+        // attemptLock();
       }
       else if (message.startsWith("STATE ")) {
         int index = message.indexOf(' ');
         if (index != -1 && index + 1 < message.length()) {
           WEBRTC_STATE = message.substring(index + 1);
-          if(WEBRTC_STATE == "Impossible") {
+          if (WEBRTC_STATE == "Impossible") {
             updateDisplay("Press the button to call");
           }
-          if(WEBRTC_STATE == "Ready") {
+          else if (WEBRTC_STATE == "Ready") {
             updateDisplay("Starting connection ...");
           }
-          if(WEBRTC_STATE == "Creating") {
+          else if (WEBRTC_STATE == "Creating") {
             updateDisplay("Connecting ...");
           }
-          if(WEBRTC_STATE == "Active") {
+          else if (WEBRTC_STATE == "Active") {
             updateDisplay("Connected, look at the camera!");
           }
         }
@@ -129,7 +152,10 @@ void setup() {
   
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), handleButtonPress, FALLING);
- 
+  
+  // Reed switch
+  // pinMode(REED_PIN, INPUT_PULLUP);
+
   // Initial locked position  
   doorServo.attach(SERVO_PIN);
   doorServo.write(0); 
@@ -166,10 +192,10 @@ void loop() {
     // wifiManager.setTimeout(180);
 
     wsConnected = false; // Reset WebSocket flag
+    updateDisplay("Connecting to WiFi ...");
 
     // Attempt to reconnect to WiFi
     wifiManager.startConfigPortal("ESP32_DoorLock");
-    updateDisplay("Connecting to WiFi ...");
     Serial.println("Connecting to WiFi ...");
     if (WiFi.status() == WL_CONNECTED) {
       Serial.println("Reconnected to WiFi");
@@ -190,6 +216,11 @@ void loop() {
   }
 
   webSocket.loop();
+
+  // Check reed switch if needed
+  // if (needLockCheck) {
+  //   attemptLock();
+  // }
 
   if (buttonPressed) {
     buttonPressed = false;
