f8f6a2c5f449711c6a608563e8324193ac3169da
Thien021103 <thien881199@gmail.com>
2025-06-25 04:44:40 +0700

    admin get group

--- signaling-server/src/routes/api.js
+++ signaling-server/src/routes/api.js
@@ -58,7 +58,7 @@ router.delete('/delete-videos', authMiddleware, handleDeleteVideo);
 router.get('/get-owners', adminAuth, handleGetOwners);
 router.delete('/delete-owner', adminAuth, handleDeleteOwner);
 
-router.get('/admin-get-group', adminAuth, handleAdminGetGroup);
+router.post('/admin-get-group', adminAuth, handleAdminGetGroup);
 router.get('/get-groups', adminAuth, handleGetGroups);
 router.post('/add-group', adminAuth, handleAddGroup);
 router.post('/send-group-id', adminAuth, handleSendGroupId);
